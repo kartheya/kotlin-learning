d33c96963b35f2c336a2a421add81e58808cf0f7
kartheya <ksingh@finishline.com>
2024-07-30 22:17:24 -0400

    Playing with Constructors

--- src/com/rsk/Person.kt
+++ src/com/rsk/Person.kt
@@ -2,22 +2,25 @@ package com.rsk
 
 //By default classes and methods in kotlin are final
 //and if you want these classes and methods to be overridden you can use "open" keyword
-abstract class Person{
+fun main(args: Array<String>) {
+    val kevin = Student("Karthik","Singh",1,"tutor for Learning Math")
+    println(kevin.firstName + " " + kevin.lastName + " " + kevin._id)
+}
 
-    var firstName:String = ""
-    var lastName:String = ""
+abstract class Person(var firstName: String, var lastName: String) {
 
     open fun getName(): String = firstName+ " " + lastName
     abstract fun getAddress(): String
+
 }
 
-class Student : Person(){
+class Student(firstName: String, lastName: String, val _id:Int,var tutor:String= "") : Person(firstName, lastName){
+
 
     override fun getAddress():String{
         return "Student Address"
     }
 
+
     override fun getName(): String = ""
-
-
 }
